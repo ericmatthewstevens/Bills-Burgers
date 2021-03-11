b5a1be393a25c3a56f7053d9ef762a1b624f6e52
SchnitzelB <es971120@icloud.com>
2021-03-10 18:10:35 -0600

    Added notes for each method/variable to Hamburger

--- Hamburger.java
+++ Hamburger.java
@@ -1,3 +1,35 @@
 public class Hamburger {
   
+  private String name;
+  private String meat;
+  private double price;
+  private String breadRollType;
+ 
+  //Separate variables for four (4) possible addition to the burger.
+  //addition1Name() - Return type: String
+  //addition1Price() - Return type: Double
+  //addition2Name() - Return type: String
+  //addition2Price() - Return type: Double
+  //addition3Name() - Return type: String
+  //addition3Price() - Return type: Double
+  //addition4Name() - Return type: String
+  //addition4Price() - Return type: Double
+
+  public Hamburger(String name, String meat, double price, String breadRollType) {
+    this.name = name;
+    this.meat = meat;
+    this.price = price;
+    this.breadRollType = breadRollType;
+  }
+
+  /*Methods to calculate hamburger additions
+  * - addHamburgerAddition1(), addHamburgerAddition2(), addHamburgerAddition3(), addHamburgerAddition4().
+  * -- These four (4) methods do NOT return values.
+  * 
+  * Method invoked to print out itemizedd listing of the final hamburger with addons, if any, and total price.
+  * - itemizehamburger().
+  * -- Return type of double.
+  * --- This includes the base price of the burger, plus any additional items.
+  */
+
 }
