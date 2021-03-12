45331940dc299588b69dda6f6335aa9cb1b68578
SchnitzelB <es971120@icloud.com>
2021-03-12 17:14:11 -0600

    Added overridden methods to DeluxeBurger class

--- Hamburger.java
+++ Hamburger.java
@@ -82,11 +82,34 @@ class DeluxeBurger extends Hamburger {
    */
 
    public DeluxeBurger() {
-     super("Deluxe Burger", "Sausage & Bacon", 19.10, "White roll");
+     super("Cheese", "Sausage & Bacon", 14.54, "White roll");
    }
 
-  //  @Override
-  //  public void addHamburgerAddition1() {
-  //    name = name + " " + addition1Name;
-  //  }
+   @Override
+   public void addHamburgerAddition1(String addition1Name, double addition1Price) {
+     System.out.println("Additional items cannot be added to a deluxe burger.");
+   }
+
+   @Override
+   public void addHamburgerAddition2(String addition2Name, double addition2Price) {
+    System.out.println("Additional items cannot be added to a deluxe burger.");
+  }
+
+  @Override
+  public void addHamburgerAddition3(String addition3Name, double addition3Price) {
+    System.out.println("Additional items cannot be added to a deluxe burger.");
+  }
+
+  @Override
+  public void addHamburgerAddition4(String addition4Name, double addition4Price) {
+    System.out.println("Additional items cannot be added to a deluxe burger.");
+  }
+
+  @Override
+  public double itemizehamburger() {
+    double chips = 2.75;
+    double drink = 1.81;
+    return ((chips + drink) + super.itemizehamburger());
+  }
+  
 }
